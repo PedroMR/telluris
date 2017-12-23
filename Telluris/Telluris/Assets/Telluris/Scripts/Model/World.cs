9214934d0a70aa4698ee79eadcf24ec80067fe18
Pedro Rabinovitch <pedromr@gmail.com>
2017-12-23 12:27:43 -0500

    Initial import

---
+++ Telluris/Telluris/Assets/Telluris/Scripts/Model/World.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+
+public class World
+{
+    public struct Config
+    {
+        public int width, height;
+    }
+
+    public enum LandType
+    {
+        Impassable,
+        Dirt,
+        Water
+    }
+
+    public class Cell
+    {
+        public int x, y;
+        public LandType landType = LandType.Dirt;
+        public int grassAmount;
+
+        public Cell(int x, int y, LandType landType)
+        {
+            this.x = x;
+            this.y = y;
+            this.landType = landType;
+            this.grassAmount = 0;
+        }
+    }
+
+    Cell[] cells;
+    private Config _config;
+    private Cell border;
+    Random random;
+
+	public Config config
+    {
+        get
+        {
+            return _config;
+        }
+    }
+
+    public void Create(Config config)
+    {
+        this._config = config;
+        cells = new Cell[config.width * config.height];
+
+        border = new Cell(-1, -1, LandType.Impassable);
+        random = new System.Random();
+
+        SpreadWater();
+        SpreadGrass(10);
+
+    }
+
+    private void SpreadGrass(int iterations = 1)
+    {
+        for (int k = 0; k < iterations; k++)
+        {
+            for (int y = 0; y < _config.height; y++)
+            {
+                for (int x = 0; x < _config.width; x++)
+                {
+                    var thisCell = GetCellAt(x, y);
+                    if (thisCell.landType != LandType.Dirt) continue;
+
+                    double chanceOfGrass = 0;
+					
+                    Cell neighbor;
+                    for (var dy = -1; dy <= +1; dy++)
+                    {
+                        for (var dx = -1; dx <= +1; dx++)
+                        {
+                            if (dy * dy + dx * dx > 1) continue;
+							neighbor = GetCellAt(x+dx, y+dy);
+							if (neighbor.grassAmount > 0) chanceOfGrass += 0.03;
+							if (neighbor.landType == LandType.Water) chanceOfGrass += 0.05;
+						}
+                    }
+
+
+					var rand = random.NextDouble();
+                    if (rand < chanceOfGrass) {
+                        thisCell.grassAmount = Math.Min(100, thisCell.grassAmount + 2);
+                    }					
+                }
+            }
+        }
+    }
+
+    internal void Tick(int iterations = 1)
+    {
+        for (int i = 0; i < iterations; i++)
+        {
+            SpreadGrass();
+        }
+    }
+
+    private void SpreadWater()
+    {
+        for (int y = 0; y < _config.height; y++)
+        {
+            for (int x = 0; x < _config.width; x++)
+            {
+				var rand = random.NextDouble();
+				var landType = (rand < 0.1f) ? LandType.Water : LandType.Dirt;
+
+				cells[GetIndexFor(x, y)] = new Cell(x, y, landType);
+            }
+        }
+    }
+
+    public Cell GetCellAt(int x, int y)
+    {
+        if (x < 0 || x >= _config.width || y < 0 || y >= _config.height)
+        {
+            return border;
+        }
+
+        int index = GetIndexFor(x, y);
+        return cells[index];
+    }
+
+    private int GetIndexFor(int x, int y)
+    {
+        return x + y * _config.width;
+    }
+}
---
+++ Telluris/Telluris/Assets/Telluris/Scripts/WorldView.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldView : MonoBehaviour {
+    [SerializeField]
+    SpriteRenderer landSpritePrefab;
+
+	World world;
+
+    List<SpriteRenderer> landSprites;
+
+    const float TIME_BETWEEN_TICKS = 0.5f;
+
+    float timeSinceLastTick = 0f;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        CreateWorld();
+        CreateLandSprites();
+    }
+    
+    private void CreateWorld()
+    {
+        var config = new World.Config();
+        config.width = 8;
+        config.height = 8;
+        world = new World();
+        world.Create(config);
+    }
+
+	private void CreateLandSprites()
+	{
+        landSprites = new List<SpriteRenderer>();
+
+        for (int y = 0; y < world.config.height; y++)
+        {
+            for (int x = 0; x < world.config.width; x++)
+            {
+                var gameObject = (GameObject)GameObject.Instantiate(landSpritePrefab.gameObject);
+                var sprite = gameObject.GetComponent<SpriteRenderer>();
+                SetSpritePosition(sprite, x, y);
+                landSprites.Add(sprite);
+            }
+        }
+        UpdateLandSprites();
+	}
+
+    private void UpdateLandSprites()
+    {
+        int index = 0;
+        for (int y = 0; y < world.config.height; y++)
+        {
+            for (int x = 0; x < world.config.width; x++)
+            {
+				var cell = world.GetCellAt(x, y);
+                var sprite = landSprites[index++].GetComponent<SpriteRenderer>();
+
+				switch (cell.landType)
+				{
+					case World.LandType.Dirt:
+						sprite.color = new Color(0.3f, 0.2f, 0.0f);
+						//if (cell.grassAmount > 0)
+						{
+							sprite.color = Color.Lerp(sprite.color, Color.green, cell.grassAmount / 100f);
+						}
+						break;
+					case World.LandType.Impassable:
+						sprite.color = new Color(0.2f, 0.2f, 0.2f);
+						break;
+					case World.LandType.Water:
+						sprite.color = new Color(0.2f, 0.2f, 0.6f);
+						break;
+				}
+			}
+        }
+    }
+
+
+
+	private void SetSpritePosition(SpriteRenderer sprite, int x, int y)
+    {
+        sprite.transform.position = new Vector3(x - world.config.width/2, y - world.config.height / 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timeSinceLastTick += Time.fixedDeltaTime;
+        while (timeSinceLastTick > TIME_BETWEEN_TICKS) {
+            timeSinceLastTick -= TIME_BETWEEN_TICKS;
+			world.Tick(1);
+        }
+        UpdateLandSprites();
+	}
+}
